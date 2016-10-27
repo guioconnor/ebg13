0a40fb3e6b5f3d038d1bf9d00d237ab1df315825
Guilherme Zühlke O'Connor <guioconnor@gmail.com>
2016-10-28 00:26:49 +0100

    algorithm implemented

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # ebg13
+
+Simple Rot13 implementation
+
+Example use
+
+```
+npm install ebg13
+```
+
+```
+var ebg13 = require('ebg13');
+
+var encodedMessage = ebg13('secret message');
+
+console.log('Decoded message:' + ebg13(encodedMessage));
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+function convertToCharCode(c) {
+  return c.charCodeAt();
+}
+
+function convertToChar(c) {
+  return String.fromCharCode(c)
+}
+
+function rotate(c) {
+  if(c>=65 && c<=90) {
+    return (c+13 <= 90) ? c+13 : c+13-90+64;
+  }
+  else if(c>=97 && c<=122) {
+    return (c+13 <= 122) ? c+13 : c+13-122+96;
+  }
+  else {
+    return c;
+  }
+}
+
+
+function rot13(message) {
+  return message.split('').map(convertToCharCode).map(rotate).map(convertToChar).join('');
+}
+
+module.exports = rot13;
